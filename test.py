ddf2b8e83fc0b0f64e5a97fee3dc5f8a6a01ea93
Den4F <sergeevdenis.py@gmail.com>
2022-06-16 02:18:02 +0300

    unittest

--- test.py
+++ test.py
@@ -1,36 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import unittest
 
-link = 'http://suninjuly.github.io/redirect_accept.html'
+class Test_class_name(unittest.TestCase):
+    def test_name(self):
+        link = 'http://suninjuly.github.io/registration1.html'
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element(By.XPATH, '//div[@class="first_block"]//input[@class="form-control first"]').send_keys('Ivan')
+        input2 = browser.find_element(By.XPATH, '//div[@class="first_block"]//input[@class="form-control second"]').send_keys('Petrov')
+        input3 = browser.find_element(By.XPATH, '//div[@class="first_block"]//input[@class="form-control third"]').send_keys('Ivan@gmail.com')
+        input4 = browser.find_element(By.XPATH, '//div[@class="second_block"]//input[@class="form-control first"]').send_keys('+128657263026')
+        input5 = browser.find_element(By.XPATH, '//div[@class="second_block"]//input[@class="form-control second"]').send_keys('New-York')
+        button = browser.find_element(By.XPATH, '//button[@type="submit"]')
+        button.click()
+        welcome_text_elt = browser.find_element(By.TAG_NAME, 'h1')
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "FAILED")
 
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
+    def test_name2(self):
+        link = 'http://suninjuly.github.io/registration2.html'
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element(By.XPATH, '//div[@class="first_block"]//input[@class="form-control first"]').send_keys('Ivan')
+        input2 = browser.find_element(By.XPATH, '//div[@class="first_block"]//input[@class="form-control second"]').send_keys('Petrov')
+        input3 = browser.find_element(By.XPATH, '//div[@class="first_block"]//input[@class="form-control third"]').send_keys('Ivan@gmail.com')
+        input4 = browser.find_element(By.XPATH, '//div[@class="second_block"]//input[@class="form-control first"]').send_keys('+128657263026')
+        input5 = browser.find_element(By.XPATH, '//div[@class="second_block"]//input[@class="form-control second"]').send_keys('New-York')
+        button = browser.find_element(By.XPATH, '//button[@type="submit"]')
+        button.click()
+        welcome_text_elt = browser.find_element(By.TAG_NAME, 'h1')
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "FAILED")
 
-    first_window = browser.window_handles[0]
-    button = browser.find_element(By.XPATH, '//button[@type="submit"]').click()
-
-    new_window = browser.window_handles[1]
-    browser.switch_to.window(new_window)
-
-    import math
-
-
-    def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
-
-
-    x_element = browser.find_element(By.ID, 'input_value')
-    x = x_element.text
-    y = calc(x)
-    z = browser.find_element(By.ID, 'answer').send_keys(y)
-    click_submit = browser.find_element(By.XPATH, '//button[@type="submit"]').click()
-
-finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
-
-# не забываем оставить пустую строку в конце файла
+if __name__ == "__main__":
+    unittest.main()
